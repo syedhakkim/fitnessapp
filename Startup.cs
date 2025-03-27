8d2b5b4da4ccb74456da1eb2227afd6289cf2cee
syedhakkim <58620235+syedhakkim@users.noreply.github.com>
2025-03-27 13:18:07 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FitnessTrackerApp.Startup))]
+
+namespace FitnessTrackerApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // You can configure auth, middleware, etc. here
+        }
+    }
+}
